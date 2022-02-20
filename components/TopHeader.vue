278f7e1c31781f65e815baf16e9ae7a5ac00b3b4
Payal17122000 <raviyapayal17@gmail.com>
2022-02-20 18:29:56 +0530

    feat: header design done

---
+++ components/TopHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="container">
+      <h3>Where in the world?</h3>
+      <button class="toggle"><fa icon="moon" />Dark Mode</button>
+    </div>
+    <div class="container">
+      <div class="form-control">
+        <i><fa icon="search" /></i>
+        <input type="text" placeholder="Search For Country..." />
+      </div>
+      <div class="dropDownCon">
+        <div class="dropDown">
+          <p>Filter By Region</p>
+          <button><fa icon="chevron-down" /></button>
+        </div>
+        <div class="drop">
+          <p>Africa</p>
+          <p>America</p>
+          <p>Asia</p>
+          <p>Europe</p>
+          <p>Oceania</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
