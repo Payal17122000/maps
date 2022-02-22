a6eea756c35555be3c04b621bc3c46f31be07a11
payal <payal@improwised.com>
2022-02-22 19:37:35 +0530

    feat: search and filter by region done

--- components/CountryData.vue
+++ components/CountryData.vue
@@ -1,24 +1,47 @@
 <template>
-  <div class="s1">
-    <div v-for="country in countryInfo" :key="country" class="countries">
-      <router-link
-        style="color: black"
-        :to="{ path: 'payal/' + country.name + '/SpecificDetail' }"
-      >
-        <div class="country">
-          <div class="country-img">
-            <img :src="country.flags.png" alt="not found" />
-          </div>
-          <div class="countryinfo">
-            <h5>
-              <b>{{ country.name }}</b>
-            </h5>
-            <p><strong>Population:</strong>{{ country.population }}</p>
-            <p><strong>Region:</strong>{{ country.region }}</p>
-            <p><strong>Capital:</strong>{{ country.capital }}</p>
-          </div>
+  <div style="box-shadow: 0 0 5px rgb(0 0 0 / 30%)">
+    <div class="container c2">
+      <div class="form-c1">
+        <i><fa class="search-icon" icon="magnifying-glass" /></i>
+        <input
+          v-model="search"
+          type="text"
+          placeholder="Search For Country..."
+        />
+      </div>
+      <div class="drop1">
+        <div class="drop2">
+          <p>filter by region</p>
+          <select v-model="regions" style="border: 0; background-color: white">
+            <option v-for="country in countryInfo" :key="country">
+              {{ country.region }}
+            </option>
+          </select>
         </div>
-      </router-link>
+      </div>
+    </div>
+
+    <div class="s1">
+      <div v-for="country in searchCountry" :key="country" class="countries">
+        <router-link
+          style="color: black"
+          :to="{ path: 'payal/' + country.name + '/SpecificDetail' }"
+        >
+          <div class="country">
+            <div class="country-img">
+              <img :src="country.flags.png" alt="not found" />
+            </div>
+            <div class="countryinfo">
+              <h5>
+                <b>{{ country.name }}</b>
+              </h5>
+              <p><strong>Population:</strong>{{ country.population }}</p>
+              <p><strong>Region:</strong>{{ country.region }}</p>
+              <p><strong>Capital:</strong>{{ country.capital }}</p>
+            </div>
+          </div>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -29,7 +52,20 @@ export default {
     return {
       countryInfo: [],
       url: 'https://restcountries.com/v2/all',
+      search: '',
+      regions: '',
     }
+  },
+  computed: {
+    searchCountry() {
+      return this.countryInfo
+        .filter((country) => {
+          return country.name.match(this.search)
+        })
+        .filter((country) => {
+          return country.region.match(this.regions)
+        })
+    },
   },
   async created() {
     const res = await this.$axios.$get(this.url)
--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -1,28 +1,31 @@
 <template>
-  <div>
+  <div style="box-shadow: 0 0 5px rgb(0 0 0 / 30%)">
     <div class="container c1">
       <h3><b>Where in the world?</b></h3>
       <button class="toggle"><fa icon="moon" />Dark Mode</button>
     </div>
-    <div class="container c1">
+    <!-- <div class="container c1">
       <div class="form-c1">
         <i><fa class="search-icon" icon="magnifying-glass" /></i>
-        <input type="text" placeholder="Search For Country..." />
+        <input v-model="search" type="text" placeholder="Search For Country..." />
       </div>
       <div class="drop1">
+        
         <div class="drop2">
+          <select style="border:0; background-color:white">
+          <option>Filter By Region</option>
+          <option v-for="country in countryInfo" :key="country">{{country.region}}</option>
+          </select>
           <p>Filter By Region</p>
           <button><fa icon="chevron-down" /></button>
-        </div>
+        
         <div class="drop3">
           <p>Africa</p>
           <p>America</p>
           <p>Asia</p>
           <p>Europe</p>
           <p>Oceania</p>
-        </div>
-      </div>
-    </div>
+        </div>  -->
   </div>
 </template>
 
@@ -32,7 +35,15 @@ export default {
     return {
       countryInfo: [],
       url: 'https://restcountries.com/v2/all',
+      search: ' ',
     }
+  },
+  computed: {
+    searchCountry() {
+      return this.countryInfo.filter((country) => {
+        return country.name.match(this.search)
+      })
+    },
   },
   async created() {
     const res = await this.$axios.$get(this.url)
